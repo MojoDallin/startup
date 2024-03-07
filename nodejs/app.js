47859e3e8a715f596139e4ccf0b2c4c5992a5260
MojoDallin <dallin.t.schmidt@gmail.com>
2024-03-06 17:09:40 -0700

    cleardata button

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('C:/Users/dalli/OneDrive/Desktop/School/CS260/startup'));
+app.use(express.static('..'));
 
 
 app.listen(port, () => {

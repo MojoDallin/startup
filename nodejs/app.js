be050085c54e5d9feedb498cafc51d4ee55f9aa2
MojoDallin <dallin.t.schmidt@gmail.com>
2024-03-07 19:49:04 -0700

    few touch ups, starting to add a global page counter

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,6 +4,13 @@ const port = 3000;
 
 app.use(express.static('..'));
 
+let counter = 0;
+
+app.get('/total-created-pages', (req, res) => {
+  counter++;
+  res.json({counter});
+});
+
 
 app.listen(port, () => {
   console.log(`Web service listening at port ${port}`);

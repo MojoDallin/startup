258bf4193beb749a18b1d44c41ca4e8f0f8bf5eb
MojoDallin <dallin.t.schmidt@gmail.com>
2024-03-17 16:23:06 -0600

    experimenting

--- database.js
+++ database.js
@@ -2,13 +2,18 @@ const { MongoClient } = require('mongodb')
 
 const url = `mongodb+srv://cs260serv:0425ds@cluster0.wcsyds8.mongodb.net/`
 const client = new MongoClient(url)
+const dbName = 'notepad'
+const colName = 'notes'
+const data = require('./noteData')
 
 async function tryToConnect()
 {
     try
     {
         await client.connect()
-        console.log("Running!")
+        const result = await client.db(dbName).collection(colName).find({"username": "tschmidt77"});
+        const cursor = await result.toArray()
+        cursor.forEach((i) => console.log(i))
     }
     finally
     {
---
+++ noteData.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        username: 'MojoDallin',
+        password: "0425ds@@",
+        pages: 7,
+        noteData: ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5', 'Page 6', 'Page 7']
+    },
+    {
+        username: 'tschmidt77',
+        password: "1177ts@@",
+        pages: 5,
+        noteData: ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']
+    }
+]
+
+module.exports = data

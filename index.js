63270cfdb0e4c515c77a322870a7940845eb4c92
MojoDallin <dallin.t.schmidt@gmail.com>
2024-03-23 13:29:14 -0600

    fixing some bugs

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const colName = 'notes'
 const uuid = require('uuid')
 const bcrypt = require('bcrypt')
 const collection = client.db('notepad').collection('notes')
-const database = require("./public/database.js")
 
 app.use(cookie());
 app.use(express.static('public'));
